efa23093b2a8969aea1cb6d2876d9aac9ddc2ed6
Atul Sharma <atul.sharma@innow8apps.com>
2021-03-15 11:15:16 +0530

    initial

---
+++ ios/Pods/Target Support Files/Pods-AwesomeTSProject-AwesomeTSProjectTests/Pods-AwesomeTSProject-AwesomeTSProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AwesomeTSProject_AwesomeTSProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_AwesomeTSProject_AwesomeTSProjectTests
+@end
